9fcc36f5e6656fa586374816f371f12834dfa95b
SimplyBLGDev <61010688+SimplyBLGDev@users.noreply.github.com>
2020-11-07 20:32:55 -0300

    Reportes UI

--- ABMC_Clientes/GUI/frmReporteClientes.Designer.cs
+++ ABMC_Clientes/GUI/frmReporteClientes.Designer.cs
@@ -33,18 +33,18 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReporteClientes));
 			this.clientesBindingSource = new System.Windows.Forms.BindingSource(this.components);
 			this.dstGeneral = new ABMC_Clientes.Data_sets.dstGeneral();
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.label2 = new System.Windows.Forms.Label();
-			this.btnSalir = new System.Windows.Forms.Button();
-			this.btnImprimir = new System.Windows.Forms.Button();
-			this.dtpFechaHasta = new System.Windows.Forms.DateTimePicker();
-			this.label1 = new System.Windows.Forms.Label();
-			this.dtpFechaDesde = new System.Windows.Forms.DateTimePicker();
 			this.rpvClientes = new Microsoft.Reporting.WinForms.ReportViewer();
 			this.clientesTableAdapter = new ABMC_Clientes.Data_sets.dstGeneralTableAdapters.ClientesTableAdapter();
+			this.gbFiltrarFechas = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.dtpFechaHasta = new System.Windows.Forms.DateTimePicker();
+			this.btnFiltrar = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.dtpFechaDesde = new System.Windows.Forms.DateTimePicker();
+			this.lblCiclos = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.dstGeneral)).BeginInit();
-			this.groupBox1.SuspendLayout();
+			this.gbFiltrarFechas.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// clientesBindingSource
@@ -57,87 +57,12 @@
 			this.dstGeneral.DataSetName = "dstGeneral";
 			this.dstGeneral.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
 			// 
-			// groupBox1
-			// 
-			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.groupBox1.Controls.Add(this.label2);
-			this.groupBox1.Controls.Add(this.btnSalir);
-			this.groupBox1.Controls.Add(this.btnImprimir);
-			this.groupBox1.Controls.Add(this.dtpFechaHasta);
-			this.groupBox1.Controls.Add(this.label1);
-			this.groupBox1.Controls.Add(this.dtpFechaDesde);
-			this.groupBox1.Location = new System.Drawing.Point(12, 311);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(661, 100);
-			this.groupBox1.TabIndex = 2;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "Filtrar por fechas";
-			this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(384, 33);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(12, 13);
-			this.label2.TabIndex = 5;
-			this.label2.Text = "y";
-			// 
-			// btnSalir
-			// 
-			this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.btnSalir.Location = new System.Drawing.Point(30, 63);
-			this.btnSalir.Name = "btnSalir";
-			this.btnSalir.Size = new System.Drawing.Size(75, 31);
-			this.btnSalir.TabIndex = 4;
-			this.btnSalir.Text = "Salir";
-			this.btnSalir.UseVisualStyleBackColor = true;
-			this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
-			// 
-			// btnImprimir
-			// 
-			this.btnImprimir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnImprimir.Location = new System.Drawing.Point(544, 63);
-			this.btnImprimir.Name = "btnImprimir";
-			this.btnImprimir.Size = new System.Drawing.Size(75, 31);
-			this.btnImprimir.TabIndex = 3;
-			this.btnImprimir.Text = "Filtrar";
-			this.btnImprimir.UseVisualStyleBackColor = true;
-			this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
-			// 
-			// dtpFechaHasta
-			// 
-			this.dtpFechaHasta.CustomFormat = "";
-			this.dtpFechaHasta.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-			this.dtpFechaHasta.Location = new System.Drawing.Point(408, 28);
-			this.dtpFechaHasta.Name = "dtpFechaHasta";
-			this.dtpFechaHasta.Size = new System.Drawing.Size(211, 20);
-			this.dtpFechaHasta.TabIndex = 2;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(14, 33);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(139, 13);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Filtrar clientes con alta entre";
-			// 
-			// dtpFechaDesde
-			// 
-			this.dtpFechaDesde.CustomFormat = "";
-			this.dtpFechaDesde.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-			this.dtpFechaDesde.Location = new System.Drawing.Point(159, 28);
-			this.dtpFechaDesde.Name = "dtpFechaDesde";
-			this.dtpFechaDesde.Size = new System.Drawing.Size(211, 20);
-			this.dtpFechaDesde.TabIndex = 0;
-			// 
 			// rpvClientes
 			// 
 			this.rpvClientes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+			this.rpvClientes.BorderStyle = System.Windows.Forms.BorderStyle.None;
 			reportDataSource1.Name = "datosTabla";
 			reportDataSource1.Value = this.clientesBindingSource;
 			this.rpvClientes.LocalReport.DataSources.Add(reportDataSource1);
@@ -145,44 +70,127 @@
 			this.rpvClientes.Location = new System.Drawing.Point(0, 0);
 			this.rpvClientes.Name = "rpvClientes";
 			this.rpvClientes.ServerReport.BearerToken = null;
-			this.rpvClientes.Size = new System.Drawing.Size(678, 303);
+			this.rpvClientes.Size = new System.Drawing.Size(678, 289);
 			this.rpvClientes.TabIndex = 3;
 			// 
 			// clientesTableAdapter
 			// 
 			this.clientesTableAdapter.ClearBeforeFill = true;
 			// 
+			// gbFiltrarFechas
+			// 
+			this.gbFiltrarFechas.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.gbFiltrarFechas.Controls.Add(this.label1);
+			this.gbFiltrarFechas.Controls.Add(this.dtpFechaHasta);
+			this.gbFiltrarFechas.Controls.Add(this.btnFiltrar);
+			this.gbFiltrarFechas.Controls.Add(this.btnExit);
+			this.gbFiltrarFechas.Controls.Add(this.dtpFechaDesde);
+			this.gbFiltrarFechas.Controls.Add(this.lblCiclos);
+			this.gbFiltrarFechas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.gbFiltrarFechas.Location = new System.Drawing.Point(12, 295);
+			this.gbFiltrarFechas.Name = "gbFiltrarFechas";
+			this.gbFiltrarFechas.Size = new System.Drawing.Size(654, 116);
+			this.gbFiltrarFechas.TabIndex = 5;
+			this.gbFiltrarFechas.TabStop = false;
+			this.gbFiltrarFechas.Text = "Filtrar por Fechas";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(301, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(15, 16);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "y";
+			// 
+			// dtpFechaHasta
+			// 
+			this.dtpFechaHasta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.dtpFechaHasta.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.dtpFechaHasta.Location = new System.Drawing.Point(322, 21);
+			this.dtpFechaHasta.Name = "dtpFechaHasta";
+			this.dtpFechaHasta.Size = new System.Drawing.Size(127, 22);
+			this.dtpFechaHasta.TabIndex = 9;
+			// 
+			// btnFiltrar
+			// 
+			this.btnFiltrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.btnFiltrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(209)))), ((int)(((byte)(29)))));
+			this.btnFiltrar.FlatAppearance.BorderSize = 0;
+			this.btnFiltrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnFiltrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnFiltrar.Image = global::ABMC_Clientes.Properties.Resources.icn_print;
+			this.btnFiltrar.Location = new System.Drawing.Point(6, 56);
+			this.btnFiltrar.Name = "btnFiltrar";
+			this.btnFiltrar.Size = new System.Drawing.Size(54, 54);
+			this.btnFiltrar.TabIndex = 8;
+			this.btnFiltrar.UseVisualStyleBackColor = false;
+			// 
+			// btnExit
+			// 
+			this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(44)))), ((int)(((byte)(27)))));
+			this.btnExit.FlatAppearance.BorderSize = 0;
+			this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnExit.Image = global::ABMC_Clientes.Properties.Resources.icn_exit;
+			this.btnExit.Location = new System.Drawing.Point(594, 56);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(54, 54);
+			this.btnExit.TabIndex = 7;
+			this.btnExit.UseVisualStyleBackColor = false;
+			// 
+			// dtpFechaDesde
+			// 
+			this.dtpFechaDesde.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.dtpFechaDesde.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.dtpFechaDesde.Location = new System.Drawing.Point(168, 21);
+			this.dtpFechaDesde.Name = "dtpFechaDesde";
+			this.dtpFechaDesde.Size = new System.Drawing.Size(127, 22);
+			this.dtpFechaDesde.TabIndex = 1;
+			// 
+			// lblCiclos
+			// 
+			this.lblCiclos.AutoSize = true;
+			this.lblCiclos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblCiclos.Location = new System.Drawing.Point(5, 26);
+			this.lblCiclos.Name = "lblCiclos";
+			this.lblCiclos.Size = new System.Drawing.Size(157, 16);
+			this.lblCiclos.TabIndex = 0;
+			this.lblCiclos.Text = "Buscar Facturados entre:";
+			// 
 			// frmReporteClientes
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(678, 423);
+			this.Controls.Add(this.gbFiltrarFechas);
 			this.Controls.Add(this.rpvClientes);
-			this.Controls.Add(this.groupBox1);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "frmReporteClientes";
 			this.Text = "Reporte Clientes";
 			this.Load += new System.EventHandler(this.frmReporteClientes_Load);
 			((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.dstGeneral)).EndInit();
-			this.groupBox1.ResumeLayout(false);
-			this.groupBox1.PerformLayout();
+			this.gbFiltrarFechas.ResumeLayout(false);
+			this.gbFiltrarFechas.PerformLayout();
 			this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button btnSalir;
-        private System.Windows.Forms.Button btnImprimir;
-        private System.Windows.Forms.DateTimePicker dtpFechaHasta;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DateTimePicker dtpFechaDesde;
         private Microsoft.Reporting.WinForms.ReportViewer rpvClientes;
         private Data_sets.dstGeneral dstGeneral;
         private System.Windows.Forms.BindingSource clientesBindingSource;
         private Data_sets.dstGeneralTableAdapters.ClientesTableAdapter clientesTableAdapter;
-    }
+		private System.Windows.Forms.GroupBox gbFiltrarFechas;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.DateTimePicker dtpFechaHasta;
+		private System.Windows.Forms.Button btnFiltrar;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.DateTimePicker dtpFechaDesde;
+		private System.Windows.Forms.Label lblCiclos;
+	}
 }
--- ABMC_Clientes/GUI/frmReporteProyectos.Designer.cs
+++ ABMC_Clientes/GUI/frmReporteProyectos.Designer.cs
@@ -27,114 +27,32 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReporteProyectos));
 			this.rpvProyectos = new Microsoft.Reporting.WinForms.ReportViewer();
 			this.Proyectos = new ABMC_Clientes.Data_sets.dstGeneral();
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-			this.btnSalir = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.btnImprimir = new System.Windows.Forms.Button();
-			this.cboUsuarios = new System.Windows.Forms.ComboBox();
 			this.proyectosTableAdapter = new ABMC_Clientes.Data_sets.dstGeneralTableAdapters.ProyectosTableAdapter();
 			this.ProyectosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			this.gbFiltrarFechas = new System.Windows.Forms.GroupBox();
+			this.cboUsuarios = new System.Windows.Forms.ComboBox();
+			this.btnFiltrar = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.lblCiclos = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.Proyectos)).BeginInit();
-			this.groupBox1.SuspendLayout();
-			this.tableLayoutPanel1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.ProyectosBindingSource)).BeginInit();
+			this.gbFiltrarFechas.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// rpvProyectos
 			// 
+			this.rpvProyectos.BorderStyle = System.Windows.Forms.BorderStyle.None;
 			this.rpvProyectos.LocalReport.ReportEmbeddedResource = "ABMC_Clientes.Reportes.ReporteProyectos.rdlc";
 			this.rpvProyectos.Location = new System.Drawing.Point(0, 0);
 			this.rpvProyectos.Name = "rpvProyectos";
 			this.rpvProyectos.ServerReport.BearerToken = null;
-			this.rpvProyectos.Size = new System.Drawing.Size(801, 301);
+			this.rpvProyectos.Size = new System.Drawing.Size(801, 321);
 			this.rpvProyectos.TabIndex = 0;
 			// 
 			// Proyectos
 			// 
 			this.Proyectos.DataSetName = "Proyectos";
 			this.Proyectos.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.groupBox1.Controls.Add(this.tableLayoutPanel1);
-			this.groupBox1.Location = new System.Drawing.Point(12, 307);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(776, 131);
-			this.groupBox1.TabIndex = 3;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "Filtrar Proyectos";
-			// 
-			// tableLayoutPanel1
-			// 
-			this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.tableLayoutPanel1.ColumnCount = 3;
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.34899F));
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.65101F));
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 182F));
-			this.tableLayoutPanel1.Controls.Add(this.btnSalir, 0, 1);
-			this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
-			this.tableLayoutPanel1.Controls.Add(this.btnImprimir, 2, 1);
-			this.tableLayoutPanel1.Controls.Add(this.cboUsuarios, 2, 0);
-			this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 33);
-			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-			this.tableLayoutPanel1.RowCount = 2;
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(764, 92);
-			this.tableLayoutPanel1.TabIndex = 7;
-			// 
-			// btnSalir
-			// 
-			this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnSalir.Location = new System.Drawing.Point(3, 49);
-			this.btnSalir.Name = "btnSalir";
-			this.btnSalir.Size = new System.Drawing.Size(153, 40);
-			this.btnSalir.TabIndex = 4;
-			this.btnSalir.Text = "Salir";
-			this.btnSalir.UseVisualStyleBackColor = true;
-			this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
-			// 
-			// label1
-			// 
-			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(162, 3);
-			this.label1.Margin = new System.Windows.Forms.Padding(3);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(416, 40);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Filtrar Proyectos por Usuarios Responsables";
-			this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
-			// 
-			// btnImprimir
-			// 
-			this.btnImprimir.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnImprimir.Location = new System.Drawing.Point(584, 49);
-			this.btnImprimir.Name = "btnImprimir";
-			this.btnImprimir.Size = new System.Drawing.Size(177, 40);
-			this.btnImprimir.TabIndex = 3;
-			this.btnImprimir.Text = "Imprimir";
-			this.btnImprimir.UseVisualStyleBackColor = true;
-			this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
-			// 
-			// cboUsuarios
-			// 
-			this.cboUsuarios.FormattingEnabled = true;
-			this.cboUsuarios.Location = new System.Drawing.Point(584, 3);
-			this.cboUsuarios.Name = "cboUsuarios";
-			this.cboUsuarios.Size = new System.Drawing.Size(121, 21);
-			this.cboUsuarios.TabIndex = 5;
 			// 
 			// proyectosTableAdapter
 			// 
@@ -145,22 +63,85 @@
 			this.ProyectosBindingSource.DataMember = "Proyectos";
 			this.ProyectosBindingSource.DataSource = this.Proyectos;
 			// 
+			// gbFiltrarFechas
+			// 
+			this.gbFiltrarFechas.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.gbFiltrarFechas.Controls.Add(this.cboUsuarios);
+			this.gbFiltrarFechas.Controls.Add(this.btnFiltrar);
+			this.gbFiltrarFechas.Controls.Add(this.btnExit);
+			this.gbFiltrarFechas.Controls.Add(this.lblCiclos);
+			this.gbFiltrarFechas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.gbFiltrarFechas.Location = new System.Drawing.Point(12, 327);
+			this.gbFiltrarFechas.Name = "gbFiltrarFechas";
+			this.gbFiltrarFechas.Size = new System.Drawing.Size(776, 111);
+			this.gbFiltrarFechas.TabIndex = 9;
+			this.gbFiltrarFechas.TabStop = false;
+			this.gbFiltrarFechas.Text = "Filtrar por Usuario";
+			// 
+			// cboUsuarios
+			// 
+			this.cboUsuarios.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.cboUsuarios.FormattingEnabled = true;
+			this.cboUsuarios.Location = new System.Drawing.Point(562, 18);
+			this.cboUsuarios.Name = "cboUsuarios";
+			this.cboUsuarios.Size = new System.Drawing.Size(208, 24);
+			this.cboUsuarios.TabIndex = 9;
+			// 
+			// btnFiltrar
+			// 
+			this.btnFiltrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.btnFiltrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(209)))), ((int)(((byte)(29)))));
+			this.btnFiltrar.FlatAppearance.BorderSize = 0;
+			this.btnFiltrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnFiltrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnFiltrar.Image = global::ABMC_Clientes.Properties.Resources.icn_print;
+			this.btnFiltrar.Location = new System.Drawing.Point(6, 51);
+			this.btnFiltrar.Name = "btnFiltrar";
+			this.btnFiltrar.Size = new System.Drawing.Size(54, 54);
+			this.btnFiltrar.TabIndex = 8;
+			this.btnFiltrar.UseVisualStyleBackColor = false;
+			// 
+			// btnExit
+			// 
+			this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(44)))), ((int)(((byte)(27)))));
+			this.btnExit.FlatAppearance.BorderSize = 0;
+			this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnExit.Image = global::ABMC_Clientes.Properties.Resources.icn_exit;
+			this.btnExit.Location = new System.Drawing.Point(716, 51);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(54, 54);
+			this.btnExit.TabIndex = 7;
+			this.btnExit.UseVisualStyleBackColor = false;
+			// 
+			// lblCiclos
+			// 
+			this.lblCiclos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.lblCiclos.AutoSize = true;
+			this.lblCiclos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblCiclos.Location = new System.Drawing.Point(383, 21);
+			this.lblCiclos.Name = "lblCiclos";
+			this.lblCiclos.Size = new System.Drawing.Size(173, 16);
+			this.lblCiclos.TabIndex = 0;
+			this.lblCiclos.Text = "Buscar planes creados por:";
+			// 
 			// frmReporteProyectos
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.gbFiltrarFechas);
 			this.Controls.Add(this.rpvProyectos);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "frmReporteProyectos";
 			this.Text = "Reporte Proyectos por Usuario";
 			this.Load += new System.EventHandler(this.frmReporteProyectos_Load);
 			((System.ComponentModel.ISupportInitialize)(this.Proyectos)).EndInit();
-			this.groupBox1.ResumeLayout(false);
-			this.tableLayoutPanel1.ResumeLayout(false);
-			this.tableLayoutPanel1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.ProyectosBindingSource)).EndInit();
+			this.gbFiltrarFechas.ResumeLayout(false);
+			this.gbFiltrarFechas.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -169,13 +150,12 @@
 
 		private Microsoft.Reporting.WinForms.ReportViewer rpvProyectos;
 		private Data_sets.dstGeneral Proyectos;
-		private System.Windows.Forms.GroupBox groupBox1;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-		private System.Windows.Forms.Button btnSalir;
-		private System.Windows.Forms.Button btnImprimir;
-		private System.Windows.Forms.ComboBox cboUsuarios;
 		private Data_sets.dstGeneralTableAdapters.ProyectosTableAdapter proyectosTableAdapter;
 		private System.Windows.Forms.BindingSource ProyectosBindingSource;
+		private System.Windows.Forms.GroupBox gbFiltrarFechas;
+		private System.Windows.Forms.ComboBox cboUsuarios;
+		private System.Windows.Forms.Button btnFiltrar;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.Label lblCiclos;
 	}
 }

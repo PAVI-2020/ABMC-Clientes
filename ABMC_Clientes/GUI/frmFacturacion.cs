5634ba659ad2ca6ab12a80f7eb36218c4086b9d9
SimplyBLGDev <61010688+SimplyBLGDev@users.noreply.github.com>
2020-11-10 15:52:36 -0300

    Facturacion verificaciones

--- ABMC_Clientes/GUI/frmFacturacion.cs
+++ ABMC_Clientes/GUI/frmFacturacion.cs
@@ -9,10 +9,8 @@ using System.Windows.Forms;
 namespace ABMC_Clientes.GUI {
     public partial class frmFacturacion : Form {
         public Usuario usuario;
-
-        private Dictionary<int, string> proyectosName;
-        private Dictionary<int, string> productosName;
-        private Dictionary<int, string> razonesSociales;
+        Verificador verificadorDetalle = new Verificador();
+        Verificador verificadorFactura = new Verificador();
 
         public frmFacturacion(Usuario usuario) {
             InitializeComponent();
@@ -25,9 +23,13 @@ namespace ABMC_Clientes.GUI {
             CargarComboOptions("CiclosPrueba", "id_ciclo_prueba, fecha_inicio_ejecucion", cboCiclo);
             this.usuario = usuario;
             txtUsuario.Text = usuario.N_usuario;
-        }
 
-        
+            verificadorDetalle.Agregar("Precio", txtPrecio);
+            verificadorDetalle.Agregar("Tipo Cobro", cboTipoCobro);
+
+            verificadorFactura.Agregar("Numero Factura", txtNumeroFactura);
+            verificadorFactura.Agregar("Cliente", cboCliente);
+        }
 
         private void CalcularTotal() {
             float total = 0;
@@ -74,88 +76,63 @@ namespace ABMC_Clientes.GUI {
 
 		private void btnAgregar_Click(object sender, EventArgs e) {
             string cobrado = "";
-            if (
-                (cboProducto.Enabled && cboProducto.SelectedIndex == -1) || 
-                (cboProyecto.Enabled && cboProyecto.SelectedIndex == -1) || 
-                (cboCiclo.Enabled && cboCiclo.SelectedIndex == -1) || 
-                (txtPrecio.Text == "") ||
-                (txtNumeroFactura.Text == "") ||
-                (cboCliente.SelectedIndex == -1) ||
-                (cboTipoCobro.SelectedIndex == -1)
-                )
-                {
-                MessageBox.Show("Complete todos los campos", "Error", MessageBoxButtons.OK);
+            if ((cboProducto.Enabled && cboProducto.SelectedIndex == -1) ||
+                (cboProyecto.Enabled && cboProyecto.SelectedIndex == -1) ||
+                (cboCiclo.Enabled && cboCiclo.SelectedIndex == -1)) {
+                MessageBox.Show("Seleccione el item a cobrar.", "Error", MessageBoxButtons.OK);
                 return;
+            } else if (verificadorDetalle.Verificar()) {
+                switch (cboTipoCobro.SelectedIndex) {
+                    case 0:
+                        cobrado = cboProducto.SelectedValue.ToString();
+                        break;
+                    case 1:
+                        cobrado = cboProyecto.SelectedValue.ToString();
+                        break;
+                    case 2:
+                        cobrado = cboCiclo.SelectedValue.ToString();
+                        break;
                 }
-            switch (cboTipoCobro.SelectedIndex) {
-                case 0:
-                    cobrado = cboProducto.SelectedValue.ToString();
-                    break;
-                case 1:
-                    cobrado = cboProyecto.SelectedValue.ToString();
-                    break;
-                case 2:
-                    cobrado = cboCiclo.SelectedValue.ToString();
-                    break;
-            }
 
-            grdDetallesFactura.Rows.Add(grdDetallesFactura.Rows.Count + 1, cboTipoCobro.Text, cobrado, txtPrecio.Text);
-            CalcularTotal();
-            cboProducto.SelectedIndex = -1;
-            cboProyecto.SelectedIndex = -1;
-            cboCiclo.SelectedIndex = -1;
+                grdDetallesFactura.Rows.Add(grdDetallesFactura.Rows.Count + 1, cboTipoCobro.Text, cobrado, txtPrecio.Text);
+                CalcularTotal();
+                cboProducto.SelectedIndex = -1;
+                cboProyecto.SelectedIndex = -1;
+                cboCiclo.SelectedIndex = -1;
+            }
 
         }
 
 		private void btnFacturar_Click(object sender, EventArgs e) {
 
             List<DetalleFactura> detalles = new List<DetalleFactura>();
-            if (
-                    grdDetallesFactura.Rows.Count == 0
-               )
-            {
+            if (grdDetallesFactura.Rows.Count == 0) {
                 MessageBox.Show("Agregue una factura previamente", "Error", MessageBoxButtons.OK);
                 return;
-            }
-            foreach (DataGridViewRow dgrid in grdDetallesFactura.Rows)
-            {
-                if ((string)dgrid.Cells[1].Value == "Por Producto") {
-                    detalles.Add(new DetalleFactura(0, 0, Convert.ToInt32(dgrid.Cells[0].Value), Convert.ToInt32(dgrid.Cells[2].Value), -1, -1, Convert.ToDecimal(dgrid.Cells[3].Value), false));
-                         }
-                else if((string)dgrid.Cells[1].Value == "Por Proyecto")
-                    {
+            } else if (verificadorFactura.Verificar()) {
+                foreach (DataGridViewRow dgrid in grdDetallesFactura.Rows) {
+                    if ((string)dgrid.Cells[1].Value == "Por Producto") {
+                        detalles.Add(new DetalleFactura(0, 0, Convert.ToInt32(dgrid.Cells[0].Value), Convert.ToInt32(dgrid.Cells[2].Value), -1, -1, Convert.ToDecimal(dgrid.Cells[3].Value), false));
+                    } else if ((string)dgrid.Cells[1].Value == "Por Proyecto") {
                         detalles.Add(new DetalleFactura(0, 0, Convert.ToInt32(dgrid.Cells[0].Value), -1, Convert.ToInt32(dgrid.Cells[2].Value), -1, Convert.ToDecimal(dgrid.Cells[3].Value), false));
+                    } else {
+                        detalles.Add(new DetalleFactura(0, 0, Convert.ToInt32(dgrid.Cells[0].Value), -1, -1, Convert.ToInt32(dgrid.Cells[2].Value), Convert.ToDecimal(dgrid.Cells[3].Value), false));
                     }
-
-                else
-                {
-                    detalles.Add(new DetalleFactura(0, 0, Convert.ToInt32(dgrid.Cells[0].Value), -1,  -1, Convert.ToInt32(dgrid.Cells[2].Value), Convert.ToDecimal(dgrid.Cells[3].Value), false));
                 }
+
+                Factura factura = new Factura(0, txtNumeroFactura.Text, Convert.ToInt32(cboCliente.SelectedValue), Convert.ToDateTime(txtFecha.Text), usuario.IdUsuario, false, cboCliente.SelectedText, txtUsuario.Text, detalles.ToArray());
+                FacturaBusiness fbus = new FacturaBusiness();
+                fbus.CrearFactura(factura);
+
+                MessageBox.Show("La factura se ha realizado correctamente", "Facturacion", MessageBoxButtons.OK);
+
+                ClearFields();
             }
-
-            
-            Factura factura = new Factura(0, txtNumeroFactura.Text, Convert.ToInt32(cboCliente.SelectedValue), Convert.ToDateTime(txtFecha.Text), usuario.IdUsuario, false, cboCliente.SelectedText, txtUsuario.Text, detalles.ToArray());
-            FacturaBusiness fbus = new FacturaBusiness();
-            fbus.CrearFactura(factura);
-
-            MessageBox.Show("La factura se ha realizado correctamente", "Facturacion", MessageBoxButtons.OK);
-
-            ClearFields();
 		}
 
         private void btnQuitar_Click(object sender, EventArgs e)
         {
             grdDetallesFactura.Rows.Remove(grdDetallesFactura.SelectedRows[0]);
-        }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void txtTotal_TextChanged(object sender, EventArgs e)
-        {
-
         }
 
         public void CargarComboOptions(string tabla, string columnas, ComboBox cmb)
